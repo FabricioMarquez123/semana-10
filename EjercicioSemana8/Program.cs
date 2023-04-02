2b0f7a7284a6c447b0870f24142aaca1fc7bee79
FabricioMarquez123 <114788319+FabricioMarquez123@users.noreply.github.com>
2023-04-01 23:17:57 -0600

    Add files via upload

---
+++ EjercicioSemana8/Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+using System;
+using EjercicioSemana8.DAO;
+using EjercicioSemana8.Models;
+
+while (true)
+
+{
+    Console.WritelLine("Por Favor Seleccione una Opcidn del Menu");
+    Console.WritelLine("1 - Guardar™");
+    Console.WritelLine("2 - Listar™");
+    Console.WritelLine("3 - Actualizar™");
+    Console.WritelLine("4 - Eliminar");
+    Console.WritelLine("5 - Salir");
+    int op = int.Parse(Console.ReadlLine());
+    Producto producto = new Producto();
+    crudProductos crudProductos = new crudProductos();
+    switch (op)
+    {
+
+        case 1:
+
+            Console.WriteLine("Ingrese el Nombre del Producto que desee llevar");
+            producto.Nombre = Console.ReadlLine();
+
+            Console.WriteLine("Ingrese la Descripcidén Producto que desee llevar");
+            producto.Descripcion = Console.ReadlLine();
+
+            Console.WriteLine("Ingrese el Precio del Producto que desee llevar");
+            producto.Precio = decimal.Parse(Console.ReadlLine());
+            Console.WriteLine("Ingrese el Stock del Producto que desee llevar");
+            producto.Stock = int.Parse(Console.ReadlLine());
+            crudProductos.guardar(producto);
+
+            break;
+        case 2:
+
+            var Lista = crudProductos.Listar();
+
+            foreach (var item in Lista)
+
+            {
+                Console.WriteLine($"ID : {item.Id}");
+                Console.WriteLine($"Nombre : {item.Nombre}");
+                Console.WritelLine($"Descripcion : {item.Descripcion}");
+                Console.WritelLine($"Precio : {item.Precio}");
+                Console.WritelLine($"Stcok : {item.Stock} \n");
+
+            }
